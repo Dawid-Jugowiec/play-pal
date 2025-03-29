104e84ef6ebd0d9a26b741c8f2ab83b73790713b
Dawid Jugowiec <d.j.w.jugowiec@gmail.com>
2025-03-29 18:38:52 +0100

    Initial UI setup.

---
+++ src/components/navbar/TopNav.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { Button, Navbar, NavbarBrand, NavbarContent } from '@heroui/react'
+import Link from 'next/link'
+import React from 'react'
+import { GiMatchTip } from 'react-icons/gi'
+import NavLink from './NavLink'
+
+export default function TopNav() {
+  return (
+    <Navbar
+        maxWidth='xl'
+        className='bg-gradient-to-r from-blue-400 to-blue-600'
+        classNames={{
+            item: [
+                'text-xl',
+                'text-white',
+                'uppercase',
+                'data-[active=true]:text-yellow-200'
+            ]
+        }}
+    >  
+        <NavbarBrand as={Link} href='/'>
+            <GiMatchTip size={40} className='text-gray-200'/>
+            <div className='font-bold text-3xl flex'>   
+                <span className='text-gray-900'>Play</span>   
+                <span className='text-gray-200'>Pal</span>   
+            </div>
+        </NavbarBrand>
+        <NavbarContent justify='center'>
+            <NavLink href='/members' label='Matches'/>
+            <NavLink href='/lists' label='Lists'/>
+            <NavLink href='/messages' label='Messages'/>
+        </NavbarContent>
+        <NavbarContent justify='end'>
+            <Button as={Link} href='/auth/login' variant='bordered' className='text-white'>
+                Login
+            </Button>
+            <Button as={Link} href='/auth/register' variant='bordered' className='text-white'>
+                Register
+            </Button>
+        </NavbarContent>
+    </Navbar>
+  )
+}
